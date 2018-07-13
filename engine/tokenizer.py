70796297c644f28a21a0bab06bb0aeb4d7af59ae
Bagus Trihatmaja <astungkara.project@gmail.com>
2018-07-13 02:13:40 -0400

    Initialize tokenizer

---
+++ engine/tokenizer.py
@@ -0,0 +1,46 @@
+import re
+from stemmer import Stemmer
+
+class Tokenizer(object):
+    """ Tokenize text
+    Any number of letters and numbers, possibly separated by single
+    periods in the middle. For instance, bob and 376 and 98.6 and 192.160.0.1
+    are all tokens. 123,456 and aunt"s are not tokens 
+    """
+    __text = ""
+    __stemmer = None
+    __stopwords = []
+
+    __pattern = r"\w+(\.?\w+)*"
+
+    def tokenize(self):
+        # Lower case all the body
+        tokens = self.__text.lower()
+        tokens = [ match.group() for match in re.finditer(self.__pattern, self.__text, re.M | re.I) ]
+
+        # Give ID to the word and remove stopwords
+        tokens = [ token for token in tokens if token not in self.__stopwords ]
+
+        # If stemmed
+        if self.__stemmer and self.__stemmer.method:
+            tokens = [ self.__stemmer.stem(token) for token in tokens ]
+        
+        return tokens
+
+
+    def __init__(self, text, stopwords_path = None, stemmer = None):
+        # If stopwords is defined
+        if stopwords_path:
+            try:
+                with open(stopwords_path, "r") as s:
+                    self.__stopwords = s.read().split("\n")
+            except Exception as exception:
+                print exception
+                
+        # If using stemmer
+        if stemmer:
+            self.__stemmer = Stemmer(stemmer)
+            if self.__stemmer.method == None:
+                raise KeyError("Stemming method not found")
+
+        self.__text = text
